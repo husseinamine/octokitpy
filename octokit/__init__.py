95995a980571f61064e6c1d9fbf75322085d5df2
me@husseinraed.cf <hussein@null.localdomain>
2020-12-23 23:39:49 +0300

    .

---
+++ octokit/__init__.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class Octokit:
+    """this object represents the github api as a python object"""
+
+    def __init__(self, token):
+        self.headers = {"Authorization": "token %s" % token}
+        self.base_url = "https://api.github.com"
+    
+    def request(self, url, body={}, **options):
+        method, path = url.split(" ")
+        result = requests.request(method, "%s%s" % (options.get("base_url") or self.base_url, path), headers={**self.headers, **(options.get("headers") or {})}, json=body, params=options.get("query"))
+        
+        try:
+            response = json.loads(result.text)
+        except ValueError:
+            return result.text
+
+        return response
