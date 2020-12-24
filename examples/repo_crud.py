35c69bf2d95f6b48684ed52c6caf6cec1ac62d67
husseinraed <me@husseinraed.cf>
2020-12-24 10:36:13 +0300

    added examples

---
+++ examples/repo_crud.py
@@ -0,0 +1,18 @@
+from octokit import Octokit
+
+octokit = Octokit("api_key")
+
+
+octokit.request("POST /user/repos", {
+    "name": "repo_name"
+}) # create repository
+
+# replace owner with the repository owner name and repo with the repository name for example /repos/husseinraed/octokit.py
+
+octokit.request("GET /repos/owner/repo") # get repository
+
+octokit.request("PATCH /repos/owner/repo", {
+    "name": "updated_name"
+}) # update repository
+
+octokit.request("DELETE /repos/owner/repo") # delete repository
